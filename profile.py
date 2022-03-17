8e8e485088a339565f4833631d6994734f2b5e56
James Li <pekwatch746@gmail.com>
2022-03-17 12:32:41 -0700

    fix a missing parenthesis

--- profile.py
+++ profile.py
@@ -49,7 +49,7 @@ pc.defineParameter(
 pc.defineParameter(
     "ricRelease","O-RAN SC RIC Release",
     portal.ParameterType.STRING,"e-release",
-    [("cherry","cherry"),("dawn","dawn"),("e-release", "e-release"],
+    [("cherry","cherry"),("dawn","dawn"),("e-release", "e-release")],
     longDescription="O-RAN SC RIC component version.  Even when you select a version, some components may be built from our own bugfix branches, and not specifically on the exact release branch.  This parameter specifies the default branch for components that we can use unmodified.")
 pc.defineParameter(
     "installORANSC","Install O-RAN SC RIC",
